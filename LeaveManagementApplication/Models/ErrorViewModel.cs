8c565f2097f59dde773443d304e6e85bebddd9cf
openkitchen2772 <53619884+openkitchen2772@users.noreply.github.com>
2022-11-29 02:16:59 +0800

    Add project files.

---
+++ LeaveManagementApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveManagementApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
